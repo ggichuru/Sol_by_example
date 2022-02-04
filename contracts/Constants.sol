adfe28497c6158991ff73d81d2d73496381685e8
ggichuru <ggichuru.dev@gmail.com>
2022-02-04 18:33:29 +0300

    Constants

---
+++ contracts/Constants.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Constants {
+    // Coding convention to uppercase constant variables
+    address public constant MY_ADDRESS =
+        0x96997E0B43A4322a8c57687f8512951123716B79;
+    uint256 public constant MY_UINT = 123;
+}
